aaef0b08c30c6b1fd00a169216838493b0ddc0e1
PankajKumar Yadav <pankaj981966@gmail.com>
2025-03-02 18:23:56 +0530

    feat: add new favicon, SVG assets, and localization files; refactor configuration and utility files

---
+++ client/src/components/providers/theme-provider.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { applyTheme, ColorMode, getTheme } from '@/utils/theme/theme';
+import { useEffect, useState } from 'react'
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+    const [colorMode, setColorMode] = useState<ColorMode>('light');
+
+    useEffect(() => {
+        const theme = getTheme();
+        // const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        // const initialColorMode: ColorMode = isDark ? 'dark' : 'light';
+        // setColorMode(initialColorMode);
+        applyTheme(theme, "light"); // set initial theme to light
+
+        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        const handleChange = (e: MediaQueryListEvent) => {
+            const newColorMode: ColorMode = e.matches ? 'dark' : 'light';
+            setColorMode(newColorMode);
+            applyTheme(theme, newColorMode);
+        };
+
+        mediaQuery.addEventListener('change', handleChange);
+        return () => mediaQuery.removeEventListener('change', handleChange);
+    }, []);
+
+    useEffect(() => {
+        document.documentElement.classList.toggle('dark', colorMode === 'dark');
+    }, [colorMode]);
+
+    return <>{children}</>;
+}
