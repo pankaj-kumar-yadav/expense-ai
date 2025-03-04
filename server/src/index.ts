e0b997ade71faf11cbc0479d0180d40b04a0770a
PankajKumar Yadav <pankaj981966@gmail.com>
2025-03-04 12:18:40 +0530

    refactor: change main entry point to TypeScript and clean up imports in index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 // import "module-alias/register";
-import express, { Request, Response } from "express";
+import { Request, Response } from "express";
 import app from "./app";
 
 const PORT = process.env.PORT || 8080;
